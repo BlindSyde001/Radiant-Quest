93bc2dca5eefa521bfe03071035171ece7380fb6
lucasOlivio <lucas27_olivio@hotmail.com>
2023-05-05 08:30:45 -0300

    fix: CHAR_PER_SECOND as const

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -7,15 +7,14 @@ using UnityEngine;
 public class UIController : MonoBehaviour
 {
     public static UIController Instance; // Singleton instance
-    public float charPerSecond;
-    
 
+    private const float CHAR_PER_SECOND = 0.4f;
     [SerializeField] private GameObject dialoguePanel;
     [SerializeField] private TextMeshProUGUI dialogueText;
     [SerializeField] private TextMeshProUGUI dialogueName;
-    
+
     private static int index;
-    private static List<string> dialogue = new List<string>{};
+    private static List<string> dialogue = new List<string> { };
 
     private void Awake()
     {
@@ -39,28 +38,33 @@ public class UIController : MonoBehaviour
 
     // DIALOGUES
     // ---------
-    public bool isDialogueActive() {
+    public bool isDialogueActive()
+    {
         return dialoguePanel.activeInHierarchy;
     }
 
-    public string GetDialogueText() {
+    public string GetDialogueText()
+    {
         return dialogueText.text;
     }
 
-    public void DialogueSetActive(bool isActive) {
+    public void DialogueSetActive(bool isActive)
+    {
         dialoguePanel.SetActive(isActive);
     }
 
-    public void StopDialogue() {
+    public void StopDialogue()
+    {
         ZeroText();
         Instance.DialogueSetActive(false);
     }
 
     // Starts a new dialogue on the scene and configures the name and list of lines for the dialogue
-    public void StartDialogue(string dialogueName, object dialogue_) {
+    public void StartDialogue(string dialogueName, object dialogue_)
+    {
         ZeroText();
         Instance.SetDialogueName(dialogueName);
-        
+
         if (dialogue_ is List<string>)
         {
             List<string> dialogueList = dialogue_ as List<string>;
@@ -69,7 +73,7 @@ public class UIController : MonoBehaviour
         else if (dialogue_ is string)
         {
             string dialogueString = dialogue_ as string;
-            dialogue = new List<string>{dialogueString};
+            dialogue = new List<string> { dialogueString };
         }
         else
         {
@@ -80,38 +84,45 @@ public class UIController : MonoBehaviour
     }
 
     // Resets the dialogues and texts
-    public void ZeroText() {
+    public void ZeroText()
+    {
         Instance.WriteDialogueText("");
         index = 0;
     }
 
     // Goes to the next line for the dialogue or stops if reached the end
-    public void NextLine() {
-        if(index < dialogue.Count)
+    public void NextLine()
+    {
+        if (index < dialogue.Count)
         {
             Instance.WriteDialogueText(dialogue[index]);
             index++;
-        } else {
+        }
+        else
+        {
             StopDialogue();
         }
     }
 
-    // Types a char from the dialog text a "charPerSecond"
-    private IEnumerator Typing(string text) {
-        foreach(char letter in text.ToCharArray())
+    // Types a char from the dialog text a "CHAR_PER_SECOND"
+    private IEnumerator Typing(string text)
+    {
+        foreach (char letter in text.ToCharArray())
         {
             dialogueText.text += letter;
-            yield return new WaitForSeconds(charPerSecond);
+            yield return new WaitForSeconds(CHAR_PER_SECOND);
         }
     }
 
-    public void WriteDialogueText(string text) {
+    public void WriteDialogueText(string text)
+    {
         dialoguePanel.SetActive(true);
         dialogueText.text = "";
         StartCoroutine(Typing(text));
     }
 
-    public void SetDialogueName(string name) {
+    public void SetDialogueName(string name)
+    {
         dialogueName.text = name;
     }
 }
