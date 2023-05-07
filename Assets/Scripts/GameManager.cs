3470a07612145244dffc3275f70ebd0d857d1de2
Samuel Jones <samuel.jones@live.com.au>
2023-05-07 19:15:39 +1000

    Changing between scenes spawning chara in right place

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using Unity.Loading;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance; // Singleton instance
     public int day; // Private variable to store the current day
+    public int playerSpawningSpot;
 
     // State of the game variables
     public enum GameStates
@@ -31,6 +34,23 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
         }
         state = GameStates.MainMenu;
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+       if(FindAnyObjectByType<PlayerController>() != null && scene.name.Contains("Town") )
+        {
+            FindAnyObjectByType<PlayerController>().transform.position = FindAnyObjectByType<LoadingSpotContainer>().spot[playerSpawningSpot].position;
+        }
     }
 
     public static int GetDay()
--- Assets/Scripts/Overworld Mechanics/Interactable/Items/CustomItems/DoorInsideController.cs
+++ Assets/Scripts/Overworld Mechanics/Interactable/Items/CustomItems/DoorInsideController.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class DoorInsideController : InteractableController
 {
+    public int playerSpawningSpot;
     protected override void Interaction()
     {
+        FindObjectOfType<GameManager>().playerSpawningSpot = playerSpawningSpot;
         ScenesManager.Instance.LoadScene($"Town_Level_Day{GameManager.GetDay() + 1}");
     }
 }
