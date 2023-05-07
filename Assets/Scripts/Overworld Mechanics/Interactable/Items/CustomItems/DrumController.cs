9c4a584cc8b519668dfd5f88e7d909c4efb0875f
lucasOlivio <lucas27_olivio@hotmail.com>
2023-05-07 07:08:36 -0300

    feat: Move drum

---
+++ Assets/Scripts/Overworld Mechanics/Interactable/Items/CustomItems/DrumController.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrumController : MonoBehaviour
+{
+    public void MoveToFinalPosition() {
+        transform.position = new Vector3(-17f, 19f, 0f);
+    }
+}
